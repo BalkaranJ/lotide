4de6da49d34a3834b6d1fc5d9b5b2b9cac8fec80
Balkaran <49322187+BalkaranJ@users.noreply.github.com>
2020-07-21 18:52:22 -0600

    Create assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,21 @@
+const eqArrays = function(arrayOne, arrayTwo){
+  if(arrayOne.length !== arrayTwo.length){
+    return false;
+  } 
+  for(let i = 0; i < arrayOne.length; i++){
+    if(arrayOne[i] !== arrayTwo[i]){
+      return false;
+    }
+  }
+  return true;
+}
+
+const assertArraysEqual = function(arrayThree, arrayFour){
+  if(eqArrays(arrayThree, arrayFour)){
+    console.log("The arrays are equal");
+  } else {
+    console.log("The arrays are not equal");
+  }
+}
+
+assertArraysEqual([1, 2, 3, 4], [1, 2, 3, 4, 5]);
