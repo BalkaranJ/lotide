c3883fe4eed857e227a8b34b42a4157b1329e02f
Balkaran <49322187+BalkaranJ@users.noreply.github.com>
2020-07-22 16:13:33 -0600

    Create letterPositions.js

---
+++ letterPositions.js
@@ -0,0 +1,13 @@
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++){
+    let char = sentence[i];
+    if(results[char]){
+      results[char].push(i);
+    } else {
+       results[char] = [i];
+    }
+  }
+  console.log(results);
+  return results;
+};
