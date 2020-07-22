1a2b1b35a592591db6f9b96ed8c9b18cca498c9e
Balkaran <49322187+BalkaranJ@users.noreply.github.com>
2020-07-21 18:25:36 -0600

    Create eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected){
+  if(actual === expected){
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arrayOne, arrayTwo){
+  if(arrayOne.length !== arrayTwo.length){
+    return false;
+  } 
+  for(let i = 0; i < arrayOne.length; i++){
+    if(arrayOne[i] !== arrayTwo[i]){
+      return false;
+    }
+  }
+  return true;
+}
+
+
+ 
+console.log(eqArrays([1, 2, 3], [1, 2, 3]));
+console.log(eqArrays([1, 2, 3], [3, 2, 1]));
