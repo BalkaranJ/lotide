257bff82e0cd80fdc0ca5d6c032267b64aef2cb4
Balkaran Jaswal <balkaranjaswal@gmail.com>
2020-07-27 20:37:12 +0000

    assertEqual, head, and tests

--- head.js
+++ head.js
@@ -1,15 +1,7 @@
-const assertEqual = function(actual, expected){
-  if(actual === expected){
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 
 let array = [1, 2, 3, 4, 5];
 const head = function(array){
   return array[0];
 }
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
