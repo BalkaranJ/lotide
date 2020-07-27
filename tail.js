622035b986489853af2fd832573bef6d582aa37c
Balkaran Jaswal <balkaranjaswal@gmail.com>
2020-07-27 20:45:54 +0000

    tail, and tail test

--- tail.js
+++ tail.js
@@ -10,6 +10,8 @@ const tail = function(arrayToSlice){
   return arrayToSlice.slice(1);
 } 
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-let newTail = tail(words); // no need to capture the return value since we are not checking it
-assertEqual(newTail.length, 3); // original array should still have 3 elements!
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// let newTail = tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(newTail.length, 3); // original array should still have 3 elements!
+
+module.exports = tail;
