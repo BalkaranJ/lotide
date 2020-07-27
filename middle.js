093bc25397b3e2ebd20accfd9def2f3b34ed343a
Balkaran Jaswal <balkaranjaswal@gmail.com>
2020-07-27 21:19:43 +0000

    middle and middleTest

--- middle.js
+++ middle.js
@@ -45,3 +45,14 @@ const middle = function(arrayOne){
     return [firstNumber, secondNumber];
   }
 }
+
+
+//TEST CODE
+// assertArraysEqual(middle([1]), []); // => []
+// assertArraysEqual(middle([1, 2]), []); // => []
+// assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
+// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
+// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
+
+module.exports = middle;
